af5465f57fd76a2cc4e54ad1853acfa18879e615
lizhen0211 <lizhen0211@163.com>
2020-03-13 10:12:34 +0800

    baseline

--- tutorial/requests/request.py
+++ tutorial/requests/request.py
@@ -1,4 +1,6 @@
 # https://requests.readthedocs.io/zh_CN/latest/
+import json
+
 import requests
 
 
@@ -103,6 +105,104 @@ def json_response_content():
     # 或者检查 r.status_code 是否和你的期望相同。
 
 
+def raw_response_content():
+    """原始响应内容"""
+    # 在罕见的情况下，你可能想获取来自服务器的原始套接字响应，那么你可以访问 r.raw。
+    # 果你确实想这么干，那请你确保在初始请求中设置了 stream=True。
+    r = requests.get('https://api.github.com/events', stream=True)
+    print(r.raw)
+
+    print(r.raw.read(10))
+
+    # 但一般情况下，你应该以下面的模式将文本流保存到文件：
+    # with open(filename, 'wb') as fd:
+    #     for chunk in r.iter_content(chunk_size):
+    #         fd.write(chunk)
+
+    # 使用 Response.iter_content 将会处理大量你直接使用 Response.raw 不得不处理的。
+    # 当流下载时，上面是优先推荐的获取内容方式。
+    # Note that chunk_size can be freely adjusted to a number that may better fit your use cases.
+
+
+def custom_headers():
+    """定制请求头"""
+    # 如果你想为请求添加 HTTP 头部，只要简单地传递一个 dict 给 headers 参数就可以了。
+    # 例如，在前一个示例中我们没有指定 content-type:
+    url = 'https://api.github.com/some/endpoint'
+    headers = {'user-agent': 'my-app/0.0.1'}
+    r = requests.get(url, headers=headers)
+    print(r)
+
+    # 注意: 定制 header 的优先级低于某些特定的信息源，例如：
+    # 如果在 .netrc 中设置了用户认证信息，使用 headers= 设置的授权就不会生效。而如果设置了 auth= 参数，``.netrc`` 的设置就无效了。
+    # 如果被重定向到别的主机，授权 header 就会被删除。
+    # 代理授权 header 会被 URL 中提供的代理身份覆盖掉。
+    # 在我们能判断内容长度的情况下，header 的 Content-Length 会被改写。
+    # 更进一步讲，Requests 不会基于定制 header 的具体情况改变自己的行为。只不过在最后的请求中，所有的 header 信息都会被传递进去。
+
+    # 注意: 所有的 header 值必须是 string、bytestring 或者 unicode。尽管传递 unicode header 也是允许的，但不建议这样做。
+
+
+def more_complicated_post_requests():
+    """更加复杂的 POST 请求"""
+    # 通常，你想要发送一些编码为表单形式的数据——非常像一个 HTML 表单。
+    # 要实现这个，只需简单地传递一个字典给 data 参数。你的数据字典在发出请求时会自动编码为表单形式：
+    payload = {'key1': 'value1', 'key2': 'value2'}
+    r = requests.post("http://httpbin.org/post", data=payload)
+    print(r.text)
+    print("==============================================")
+    # 你还可以为 data 参数传入一个元组列表。
+    # 在表单中多个元素使用同一 key 的时候，这种方式尤其有效：
+    payload = (('key1', 'value1'), ('key1', 'value2'))
+    r = requests.post('http://httpbin.org/post', data=payload)
+    print(r.text)
+    print("==============================================")
+    # 很多时候你想要发送的数据并非编码为表单形式的。
+    # 如果你传递一个 string 而不是一个 dict，那么数据会被直接发布出去。
+    url = 'http://httpbin.org/post'
+    payload = {'some': 'data'}
+    r = requests.post(url, data=json.dumps(payload))
+    print(r.text)
+    print("==============================================")
+    # 此处除了可以自行对 dict 进行编码，你还可以使用 json 参数直接传递，然后它就会被自动编码。
+    # 这是 2.4.2 版的新加功能：
+    url = 'http://httpbin.org/post'
+    payload = {'some': 'data'}
+    r = requests.post(url, json=payload)
+    print(r.text)
+    print("==============================================")
+
+
+def post_a_multipart_encoded_file():
+    """POST一个多部分编码(Multipart-Encoded)的文件"""
+    # Requests 使得上传多部分编码文件变得很简单：
+    url = 'http://httpbin.org/post'
+    files = {'file': open('../../../report.xls', 'rb')}
+    r = requests.post(url, files=files)
+    print(r.text)
+    print("==============================================")
+    # 你可以显式地设置文件名，文件类型和请求头：
+    url = 'http://httpbin.org/post'
+    files = {'file': ('report.xls', open('../../../report.xls', 'rb'), 'application/vnd.ms-excel', {'Expires': '0'})}
+    r = requests.post(url, files=files)
+    print(r.text)
+    print("==============================================")
+    # 如果你想，你也可以发送作为文件来接收的字符串：
+    url = 'http://httpbin.org/post'
+    files = {'file': ('report.csv', 'some,data,to,send\nanother,row,to,send\n')}
+    r = requests.post(url, files=files)
+    print(r.text)
+    print("==============================================")
+    # 如果你发送一个非常大的文件作为 multipart/form-data 请求，你可能希望将请求做成数据流。
+    # 默认下 requests 不支持, 但有个第三方包 requests-toolbelt 是支持的。
+    # 你可以阅读 toolbelt（链接） 文档 来了解使用方法。
+    # 在一个请求中发送多文件参考 高级用法（链接） 一节。
+    # 警告:
+    # 我们强烈建议你用二进制模式(binary mode)打开文件。
+    # 这是因为 Requests 可能会试图为你提供 Content-Length header，在它这样做的时候，这个值会被设为文件的字节数（bytes）。
+    # 如果用文本模式(text mode)打开文件，就可能会发生错误。
+
+
 # 一个简单的demo
 # simple_request()
 
@@ -119,4 +219,16 @@ def json_response_content():
 # binary_response_content()
 
 # JSON 响应内容
-json_response_content()
+# json_response_content()
+
+# 原始响应内容
+# raw_response_content()
+
+# 定制请求头
+# custom_headers()
+
+# 更加复杂的 POST 请求
+# more_complicated_post_requests()
+
+# POST一个多部分编码(Multipart-Encoded)的文件
+post_a_multipart_encoded_file()
